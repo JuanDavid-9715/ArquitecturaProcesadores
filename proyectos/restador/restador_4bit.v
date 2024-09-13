dd4d9f0762341b7be369170f631b452d136e961d
EsnetT <julianes.riverosac@ecci.edu.co>
2024-09-13 01:01:30 -0500

    Actulizacion restador

--- proyectos/restador/restador_4bit.v
+++ proyectos/restador/restador_4bit.v
@@ -5,16 +5,18 @@ module restador_4bit(
     input [3:0] B,
     input       select,
     output Cout,
-    output [3:0] S
+    output   [3:0] S
 );
 
-wire [3:0] Bx;
+    wire [3:0] Bx;
+    wire [3:0] s1;
 
-xor(Bx[0], B[0], select);
-xor(Bx[1], B[1], select);
-xor(Bx[2], B[2], select);
-xor(Bx[3], B[3], select);
+    xor(Bx[0], B[0], select);
+    xor(Bx[1], B[1], select);
+    xor(Bx[2], B[2], select);
+    xor(Bx[3], B[3], select);
 
-sumador_4bit s0(.A(A), .B(Bx), .Ci(select), .Cout(Cout), .Sum(S));
+    sumador_4bit s0(.A(A), .B(Bx), .Ci(select), .Cout(Cout), .Sum(s1));
 
+    assign S = (Cout) ? s1 : ~s1 | 1;
 endmodule
