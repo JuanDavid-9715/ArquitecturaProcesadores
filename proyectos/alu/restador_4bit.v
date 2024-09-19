c0324ab59dc1187fbc58374ea79cef33fad9f8d6
stevengaona <michaels.gaonam@ecci.edu.co>
2024-09-18 19:35:50 -0500

    actualizacion de ALU

---
+++ proyectos/alu/restador_4bit.v
@@ -0,0 +1,22 @@
+`include "sumador_4bit.v"
+
+module restador_4bit(
+    input [3:0] A,
+    input [3:0] B,
+    input       select,
+    output Cout,
+    output   [3:0] S
+);
+
+    wire [3:0] Bx;
+    wire [3:0] s1;
+
+    xor(Bx[0], B[0], select);
+    xor(Bx[1], B[1], select);
+    xor(Bx[2], B[2], select);
+    xor(Bx[3], B[3], select);
+
+    sumador_4bit s0(.A(A), .B(Bx), .Ci(select), .Cout(Cout), .Sum(s1));
+
+    assign S = (Cout) ? s1 : ~s1 + 1'b1;
+endmodule
---
+++ proyectos/alu/sumador_4bit.v
@@ -0,0 +1,16 @@
+`include "sumador_1bit.v"
+
+module sumador_4bit(
+        input  [3:0] A,
+        input  [3:0] B,
+        output       Cout,
+        output [3:0] Sum
+    );
+
+    wire c1,c2,c3;
+      
+    sumador_1bit s0 (.A(A[0]), .B(B[0]), .Ci(1'b0), .Cout(c1), .Sum(Sum[0]));
+    sumador_1bit s1 (.A(A[1]), .B(B[1]), .Ci(c1), .Cout(c2), .Sum(Sum[1]));
+    sumador_1bit s2 (.A(A[2]), .B(B[2]), .Ci(c2), .Cout(c3), .Sum(Sum[2]));
+    sumador_1bit s3 (.A(A[3]), .B(B[3]), .Ci(c3), .Cout(Cout), .Sum(Sum[3]));
+endmodule
